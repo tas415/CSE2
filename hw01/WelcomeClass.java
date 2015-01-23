37c9f415afea386ee3d178ac87ff3257acdf3e50
tas415 <tas415@lehigh.edu>
2015-01-23 22:39:44 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,33 @@
+/////////////////
+//Travis Smith
+//hw01
+//Welcome Class Java Program
+//
+//  Change every "\" to "\\"
+//
+//  compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+
+//  pring the statement after making the appropriate changes
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-T--A--S--4--1--5-> ");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println(" I am a senior in the IBE program getting a B.S. IBE Finance");
+    System.out.println(" degree and a B.S. Industrial & Systems Engineering degree. ");
+    
+    //test/testtest
+    }
+}
